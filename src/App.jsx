d912e260a12ce71225080a1e578c1dcb2b65b8d1
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-10 15:40:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-mport { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNews } from './hooks/useNews'
 import useGeoLocation from './hooks/useGeoLocation'
